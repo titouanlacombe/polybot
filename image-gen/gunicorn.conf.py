570e7e60f71c00a7bded906a64f142fe1f5c1d4f
Titouan Lacombe <titouan.lacombe99@gmail.com>
2022-09-21 09:47:27 +0200

    added logs

--- image-gen/gunicorn.conf.py
+++ image-gen/gunicorn.conf.py
@@ -6,6 +6,9 @@ reload=True
 
 # max_requests=3
 # max_requests_jitter=2
-# capture_output=True
+capture_output=True
 
+loglevel="info"
+accesslog="../data/logs/image-gen.log"
+errorlog="../data/logs/image-gen.log"
 access_log_format = "%(r)s responded %(s)s in %(M)s ms"
