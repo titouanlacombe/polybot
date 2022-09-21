5b64df10dbaa808a30443fa67f11668938bbd0f6
Titouan Lacombe <titouan.lacombe99@gmail.com>
2022-09-21 15:02:04 +0200

    added kwargs depending on gpu arch

--- image-gen/setup_pipeline.py
+++ image-gen/setup_pipeline.py
@@ -2,17 +2,38 @@ import os
 from diffusers import StableDiffusionPipeline
 import torch
 
+compute_device = os.getenv("COMPUTE_DEVICE", "cpu")
+
+# TODO how to fix fp16 on the CPU
+if compute_device == "cpu":
+	device = torch.device("cpu")
+	kwargs = {
+		"revision": "fp16",
+		"torch_dtype": torch.bfloat16
+	}
+elif compute_device == "cuda":
+	device = torch.device("cuda")
+	# Optimization because i have no RAM :'(
+	kwargs = {
+		"revision": "fp16",
+		"torch_dtype": torch.float16
+	}
+elif compute_device == "rocm":
+	device = torch.device("rocm")
+	kwargs = {
+		"revision": "fp16",
+		"torch_dtype": torch.float16
+	}
+else:
+	raise Exception(f"Unknown compute_device: {compute_device}")
+
 print("Setting up pipeline, caching pretrained model")
 
 pipe = StableDiffusionPipeline.from_pretrained(
 	"CompVis/stable-diffusion-v1-4",
 	use_auth_token=os.getenv("HF_TOKEN"),
 	cache_dir="../data/hf_cache/models",
-
-	# Optimization because i have no RAM :'(
-	# TODO how to fix fp16 on the CPU
-	revision="fp16",
-	torch_dtype=torch.float16,
+	**kwargs
 )
 
 print("Pipeline setup complete")
