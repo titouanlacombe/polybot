f86eedb73350dd616f564f7904c7332cd8a42087
Titouan Lacombe <titouan.lacombe99@gmail.com>
2022-10-14 20:52:45 +0200

    fast shutdown

--- image-gen/Worker.py
+++ image-gen/Worker.py
@@ -1,9 +1,25 @@
 import logging
 logging.basicConfig(filename="../data/logs/image-gen.log", level=logging.INFO)
 
-import threading
+import signal, threading, torch
+
 from Flask import app
+from Config import device
 from PipelineLoader import load_pipeline
+
+def close(signum, frame):
+	app.logger.info("Stopping worker")
+	
+	if app.config["pipeline"] is not None:
+		del app.config["pipeline"]
+
+	if device.type != "cpu":
+		torch.cuda.empty_cache()
+	
+	app.logger.info("Worker stopped")
+	exit(0)
+
+signal.signal(signal.SIGTERM, close)
 
 # Start a daemon thread to load the pipeline
 thread = threading.Thread(target=load_pipeline, args=(app.config,), daemon=True)
