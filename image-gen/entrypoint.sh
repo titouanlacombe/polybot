5eb15ece44150f7fc7a63cbc36cd770b4c29cb46
Titouan Lacombe <titouan.lacombe99@gmail.com>
2022-10-14 18:14:07 +0200

    pytorch patch

--- image-gen/entrypoint.sh
+++ image-gen/entrypoint.sh
@@ -2,4 +2,7 @@
 # Hugging face cache fix
 export TRANSFORMERS_CACHE="../data/hf_cache/transformers"
 
+# Pytorch patch
+export HSA_OVERRIDE_GFX_VERSION=10.3.0
+
 exec gunicorn -c gunicorn.conf.py Worker:app
