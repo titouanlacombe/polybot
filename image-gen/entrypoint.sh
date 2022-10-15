27e2a1cb1d8b35c2a3a09bb5a6776d057800c74a
Titouan Lacombe <titouan.lacombe99@gmail.com>
2022-10-15 12:38:50 +0200

    tweak

--- image-gen/entrypoint.sh
+++ image-gen/entrypoint.sh
@@ -2,7 +2,4 @@
 # Hugging face cache fix
 export TRANSFORMERS_CACHE="../data/hf_cache/transformers"
 
-# Pytorch patch for recent AMD cards
-export HSA_OVERRIDE_GFX_VERSION=10.3.0
-
 exec gunicorn -c gunicorn.conf.py Worker:app
