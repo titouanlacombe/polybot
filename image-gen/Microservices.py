038914f9290d2ab710e1b3b264c76e623367fc03
Titouan Lacombe <titouan.lacombe99@gmail.com>
2022-10-16 10:03:16 +0200

    implemented async version of call service

--- image-gen/Microservices.py
+++ image-gen/Microservices.py
@@ -1,9 +1,10 @@
-import logging, requests, os
+import asyncio, logging, aiohttp, os
 
 log = logging.getLogger(__name__)
 
-def call_rpc(host: str, command: str, *args, **kwargs):
-	url = f"http://{host}/rpc"
+session = aiohttp.ClientSession()
+
+async def async_call_rpc(host: str, command: str, *args, **kwargs):
 	data = {
 		"command": command,
 		"args": args,
@@ -11,13 +12,21 @@ def call_rpc(host: str, command: str, *args, **kwargs):
 	}
 
 	log.info(f"Calling {host}: {data}")
-	response: dict = requests.post(url, json=data).json()
-	log.info(f"Response from {host}: {response}")
+
+	async with session.post(f"http://{host}/rpc", json=data) as resp:
+		if resp.status < 200 or resp.status >= 300:
+			raise Exception(f"RPC failed with status {resp.status}: {await resp.text()}")
+		
+		response: dict = await resp.json()
+		log.info(f"Response from {host}: {response}")
 	
 	if response.get("error") is not None:
 		raise Exception(response["error"])
 
 	return response["result"]
+
+def call_rpc(host: str, command: str, *args, **kwargs):
+	return asyncio.run(async_call_rpc(host, command, *args, **kwargs))
 
 polybot_host = f"bot:{os.environ['POLYBOT_PORT']}"
 polybot_api_host = f"bot:{os.environ['POLYBOT_API_PORT']}"
