a6a8b201a2309ab198ba77d3a8eb824dcd8b53b0
Titouan Lacombe <titouan.lacombe99@gmail.com>
2022-10-16 10:04:41 +0200

    fix

--- image-gen/Worker.py
+++ image-gen/Worker.py
@@ -26,13 +26,12 @@ signal.signal(signal.SIGTERM, close)
 # Start a daemon thread to load the pipeline
 thread = threading.Thread(target=load_pipeline, args=(app.config,), daemon=True)
 thread.start()
+app.config["pipeline_loader_thread"] = thread
 
 # Start a event loop daemon thread
 event_loop = asyncio.new_event_loop()
 thread = threading.Thread(target=event_loop.run_forever, daemon=True)
 thread.start()
 app.config["event_loop"] = event_loop
-
-app.config["pipeline_loader_thread"] = thread
 
 app.logger.info("Worker started")
