d7756d1e3a1667f2a2e34d76ecd087b0661abb81
Titouan Lacombe <titouan.lacombe99@gmail.com>
2023-03-05 11:05:31 +0100

    cddccddc

--- src/config/App.py
+++ src/config/App.py
@@ -12,12 +12,6 @@ bot_token = os.getenv("BOT_TOKEN")
 (polybot_host, polybot_port) = ("0.0.0.0", 5000)
 (api_host, api_port) = ("api", 5000)
 
-api_port = 443
-if in_dev: api_host = "sta.polybot.lol"
-elif in_sta: api_host = "sta.polybot.lol"
-elif in_pre: api_host = "pre.polybot.lol"
-elif in_pro: api_host = "polybot.lol"
-
 command_prefix = "/"
 activity_update_interval = 15*60 # seconds
 
@@ -28,6 +22,7 @@ api_password = os.getenv("API_PASSWORD")
 if api_password is None:
     raise Exception("API_PASSWORD is not set")
 
-api_url = f"https://{api_host}:{api_port}"
+# Not https because we are in the internal network
+api_url = f"http://{api_host}:{api_port}"
 auth_token = base64.b64encode(f"{api_account}:{api_password}".encode()).decode()
 auth_header = {"Authorization": f"Basic {auth_token}"}
